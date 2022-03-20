5cffd566c18f3db60820c4a715efd56b577b9bef
YigitCanBUDAK <budakyigitcan26@gmail.com>
2022-03-20 03:06:40 +0300

    Initial commit

---
+++ src/main/java/day1/SeleniumDemo.java
@@ -0,0 +1,21 @@
+package day1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumDemo {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\can\\Desktop\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://facebook.com");
+        System.out.println(driver.getTitle());
+        driver.quit();
+
+
+
+    }
+
+}
